2446c554ebc160e9aa59136372fb8255d0d5daff
André <andreglock@gmail.com>
2021-07-09 09:10:07 +0200

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,8 +8,17 @@ let days = document.getElementById("days");
 let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
+const h1 = document.querySelector("h1");
 
-let happening = moment("20211026 00:00:00", "YYYYMMDD hh:mm:ss");
+let happening = moment("19901026 00:00:00", "YYYYMMDD hh:mm:ss");
+
+while (happening.diff(moment(new Date())) <= 0) {
+    happening.add(1, 'years');
+}
+const yearHappening = moment(happening).format("YYYY");
+const birthday = yearHappening - 1989;
+
+h1.innerText = `The ${humanize(birthday)} countdown!`
 
 days.innerText = happening.diff(moment(new Date()), 'days');
 hours.innerText = (happening.diff(moment(new Date()), 'hours') % 24);
@@ -22,3 +31,24 @@ setInterval(function(){
     minutes.innerText = (happening.diff(moment(new Date()), 'minutes') % 60);
     seconds.innerText = (happening.diff(moment(new Date()), 'seconds') % 60);
  }, 1000);
+
+
+
+ function humanize(number) {
+    if (typeof number !== "number") {
+        return "Invalid input";
+    }
+    if (number % 1 !== 0) {
+        return "Invalid input";
+    }
+    switch (number.toFixed().slice(-1)) {
+        case "1":
+            return `${number}st`;
+        case "2":
+            return `${number}nd`;
+        case "3":
+            return `${number}rd`;
+        default:
+            return `${number}th`;
+    }
+}
